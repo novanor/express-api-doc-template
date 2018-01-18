411d42797e1a3ac3a1318024560fe9b78f70e76a
Marin Aldimirov <marin.aldimirov@novanor.org>
2018-01-18 14:50:55 +0200

    Add headers to examples

--- frontend/views/example.js
+++ frontend/views/example.js
@@ -5,7 +5,7 @@ const ExampleItem = Marionette.View.extend({
   template,
 
   ui: {
-    item:    '.dt-example-item',
+    item: '.dt-example-item',
     example: '.dt-example-content',
   },
 
@@ -19,13 +19,15 @@ const ExampleItem = Marionette.View.extend({
 
   serializeData() {
     return {
-      model:    this.model,
-      url:      this.model.get('url'),
-      status:   this.model.get('status'),
-      type:     this.model.get('type'),
-      method:   this.model.get('method'),
-      request:  JSON.stringify(this.model.get('request'), null, 2),
-      response: JSON.stringify(this.model.get('response'), null, 2),
+      model: this.model,
+      url: this.model.get('url'),
+      status: this.model.get('status'),
+      type: this.model.get('type'),
+      method: this.model.get('method'),
+      requestHeaders: JSON.stringify(this.model.get('request').headers, null, 2),
+      requestBody: JSON.stringify(this.model.get('request').body, null, 2),
+      responseHeaders: JSON.stringify(this.model.get('response').headers, null, 2),
+      responseBody: JSON.stringify(this.model.get('response').body, null, 2),
     };
   },
 });
